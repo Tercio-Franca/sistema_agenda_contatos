757c9e78b4a83708591840225ed68872fc0c37cb
tercio_franca <tercio.franca@hotmail.com>
2022-11-28 22:35:46 -0300

    Criação dos Mutators e Accessors

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -32,6 +32,70 @@ class Contato extends Model
     ];
 
     /**
+     * Get the categoria attribute.
+     *
+     * @return string
+     */
+    public function getCategoriaAttribute() {
+        return $this->categoriaRelationship;
+    }
+
+    /**
+     * Get the endereco attribute.
+     *
+     * @return string
+     */
+    public function getEnderecoAttribute() {
+        return $this->enderecoRelationship;
+    }
+
+    /**
+     * Get the telefone attribute.
+     *
+     * @return string
+     */
+    public function getTelefoneAttribute() {
+        return $this->telefoneRelationship;
+    }
+
+    /**
+     * Set the categoria attribute.
+     *
+     * @param  array  $value
+     * @return void
+     */
+    public function setCategoriaAttribute($value) {
+        $this->categoriaRelationship()->sync($value);
+    }
+
+    /**
+     * Set the endereco's id.
+     *
+     * @param  int  $value
+     * @return void
+     */
+    public function setEnderecoAttribute($value)
+    {
+        if(isset($value)){
+            $this->attributes['endereco_id'] = Endereco::where('id', $value)->first()->id;
+        }
+    }
+
+    /**
+     * Set the telefone's id.
+     *
+     * @param  int  $value
+     * @return void
+     */
+    public function setTelefoneAttribute($value)
+    {
+        if(isset($value)){
+            $this->attributes['telefone_id'] = Telefone::where('id', $value)->first()->id;
+        }
+    }
+
+
+    /**
      * The Categorias that belong to the contato.
      *
      * @return Categoria
