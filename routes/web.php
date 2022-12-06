acf3237d8e469eeb89fa0dda22de0ec1dcb04c09
terciofranca <tercio.franca@hotmail.com>
2022-12-06 11:06:28 -0300

    Criação da rota do método edit do controlador de contatos

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/index', [ContatoController::class, 'index'])->name('contatos.index'
 Route::get('/create', [ContatoController::class, 'create'])->name('contatos.create');
 Route::post('/store', [ContatoController::class, 'store'])->name('contatos.store');
 Route::get('/show/{id}', [ContatoController::class, 'show'])->name('contatos.show');
+Route::get('/edit/{id}', [ContatoController::class, 'edit'])->name('contatos.edit');
+
