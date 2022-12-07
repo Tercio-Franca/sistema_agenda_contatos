f9c7c58d8bd5fdb243223a1fa160cde9bcb7124f
terciofranca <tercio.franca@hotmail.com>
2022-12-07 11:58:10 -0300

    Criação da rota delete do módulo de Contatos

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::post('/store', [ContatoController::class, 'store'])->name('contatos.store
 Route::get('/show/{id}', [ContatoController::class, 'show'])->name('contatos.show');
 Route::get('/edit/{id}', [ContatoController::class, 'edit'])->name('contatos.edit');
 Route::post('/update/{id}', [ContatoController::class, 'update'])->name('contatos.update');
+Route::delete('/destroy/{id}', [ContatoController::class, 'destroy'])->name('contatos.destroy');
 
