2324ec5baca4400a88223a1fc59493fc0c79df03
terciofranca <tercio.franca@hotmail.com>
2022-12-07 11:51:02 -0300

    Criação da rota de update

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/create', [ContatoController::class, 'create'])->name('contatos.crea
 Route::post('/store', [ContatoController::class, 'store'])->name('contatos.store');
 Route::get('/show/{id}', [ContatoController::class, 'show'])->name('contatos.show');
 Route::get('/edit/{id}', [ContatoController::class, 'edit'])->name('contatos.edit');
+Route::post('/update/{id}', [ContatoController::class, 'update'])->name('contatos.update');
 
