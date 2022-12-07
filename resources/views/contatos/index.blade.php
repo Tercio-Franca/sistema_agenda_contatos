63d1dd62593bc260510a1722a699d14565637d28
terciofranca <tercio.franca@hotmail.com>
2022-12-07 14:14:32 -0300

    Customização inicial da view index de Contatos

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -1,2 +1,30 @@
-<!DOCTYPE html>
-{{$contatos}}
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>SAC - Listagem de Contatos</title>
+    </head>
+    <body>
+        <h1>Listagem de Contatos</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Telefone</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($contatos as $contato)
+                <tr>
+                    <td>{{$contato->nome}}</td>
+                    <td>{{$contato->categoria}}</td>
+                    <td>{{$contato->telefone}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+    </body>
+</html>
