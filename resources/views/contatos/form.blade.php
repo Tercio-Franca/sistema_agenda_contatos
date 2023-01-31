e1b9a22f23d65b5db20213b00ff2292c43718a8b
terciofranca <tercio.franca@hotmail.com>
2023-01-31 13:30:39 -0300

    sombras, cards, link e novas divs

--- resources/views/contatos/form.blade.php
+++ resources/views/contatos/form.blade.php
@@ -14,6 +14,15 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
+        <h1>
+            <a href="{{route('contatos.index')}}">
+                @if(isset($contato))
+                    SAC - Visualização do Contato
+                @else
+                    SAC - Criação de Contato
+                @endif
+            </a>
+        </h1>
         @csrf
 
         @if (isset($contato))
@@ -21,50 +30,56 @@
         @else
             {!! Form::open(['route' => 'contatos.store', 'method' => 'POST', 'name' => 'form']) !!}
         @endif
-        <div class="card container" >
-            <div class= "card container">
-                {!!Form::label('nome', 'Nome:', ['class' => 'form-check-label'])!!}
-                {!!Form::text('nome', isset($contato) ? $contato->nome : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
-            </div>
-            <div class= "card container">
-                <h4> Endereço: </h4>
-                {!!Form::label('logradouro', 'Logradouro:', ['class' => 'form-check-label'])!!}
-                {!!Form::text('logradouro', isset($contato) ? $contato->endereco->logradouro : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
 
-                {!!Form::label('numero', 'Número:', ['class' => 'form-check-label'])!!}
-                {!!Form::text('numero', isset($contato) ? $contato->endereco->numero : null, ['class' => 'form-control','placeholder' => 'Somente Números',  $form??null])!!}
-
-                {!!Form::label('cidade', 'Cidade:', ['class' => 'form-check-label'])!!}
-                {!!Form::text('cidade', isset($contato) ? $contato->endereco->cidade : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
-            </div>
-            <div class= "card container">
-                <h4> Telefones: </h4>
-                {{-- {{dd($contato->telefone)}} --}}
-                {!!Form::label('telefone[]', 'Telefone 1:', ['class' => 'form-check-label'])!!}
-                {!!Form::select('telefone[]', $telefones, isset($contato) && ($contato->telefone->get(0) !== null) ? $contato->telefone->get(0)->id : null, ['class' => 'form-control', 'placeholder' => 'Selecione um Telefone', $form??null])!!}
-
-                {!!Form::label('telefone[]', 'Telefone 2:', ['class' => 'form-check-label'])!!}
-                {!!Form::select('telefone[]', $telefones, isset($contato) && ($contato->telefone->get(1) !== null) ? $contato->telefone->get(1)->id : null, ['class' => 'form-control', 'placeholder' => 'Selecione um Telefone', $form??null])!!}
-            </div>
-            <div class= "card container">
-                <h4> Categorias: </h4>
-                <div>
-                @foreach($categorias as $categoria)
-                    {!! Form::label("categoria[]", $categoria, ['class' => 'labelmargem']) !!}
-                    {!! Form::checkbox("categoria[]", $loop->iteration, isset($contato) ? $contato->categoria->find($loop->iteration) !== null: null, ['id' => "categoria$loop->iteration", isset($form) ? $form : null]) !!}
-                @endforeach
+        <div class="card container">
+            @if (isset($contato))
+                <h2 class="card-header"> Contato: {{$contato->nome}} </h2>
+            @endif
+                <div class= "card container">
+                    {!!Form::label('nome', 'Nome:', ['class' => 'form-check-label'])!!}
+                    {!!Form::text('nome', isset($contato) ? $contato->nome : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
                 </div>
-            </div>
+                <div class= "card container">
+                    <h4> Endereço: </h4>
+                    {!!Form::label('logradouro', 'Logradouro:', ['class' => 'form-check-label'])!!}
+                    {!!Form::text('logradouro', isset($contato) ? $contato->endereco->logradouro : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
+
+                    {!!Form::label('numero', 'Número:', ['class' => 'form-check-label'])!!}
+                    {!!Form::text('numero', isset($contato) ? $contato->endereco->numero : null, ['class' => 'form-control','placeholder' => 'Somente Números',  $form??null])!!}
+
+                    {!!Form::label('cidade', 'Cidade:', ['class' => 'form-check-label'])!!}
+                    {!!Form::text('cidade', isset($contato) ? $contato->endereco->cidade : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
+                </div>
+                <div class= "card container">
+                    <h4 class= "teste"> Telefones: </h4>
+                    {{-- {{dd($contato->telefone)}} --}}
+                    {!!Form::label('telefone[]', 'Telefone 1:', ['class' => 'form-check-label'])!!}
+                    {!!Form::select('telefone[]', $telefones, isset($contato) && ($contato->telefone->get(0) !== null) ? $contato->telefone->get(0)->id : null, ['class' => 'form-control', 'placeholder' => 'Selecione um Telefone', $form??null])!!}
+
+                    {!!Form::label('telefone[]', 'Telefone 2:', ['class' => 'form-check-label'])!!}
+                    {!!Form::select('telefone[]', $telefones, isset($contato) && ($contato->telefone->get(1) !== null) ? $contato->telefone->get(1)->id : null, ['class' => 'form-control', 'placeholder' => 'Selecione um Telefone', $form??null])!!}
+                </div>
+                <div class= "card container">
+                    <h4> Categorias: </h4>
+                    <div>
+                    @foreach($categorias as $categoria)
+                        {!! Form::label("categoria[]", $categoria, ['class' => 'labelmargem']) !!}
+                        {!! Form::checkbox("categoria[]", $loop->iteration, isset($contato) ? $contato->categoria->find($loop->iteration) !== null: null, ['id' => "categoria$loop->iteration", isset($form) ? $form : null]) !!}
+                    @endforeach
+                    </div>
+                </div>
 
             <div class= "container">
-                {!! Form::submit('Salvar', ['class' => 'btn btn-success', $form??null]) !!}
+
+                {!! Form::submit('Salvar', ['class' => 'btn form btn-success', $form??null]) !!}
                 {!! Form::close() !!}
 
                 @if (isset($contato))
                     {!! Form::open(['route' => ['contatos.destroy', $contato->id], 'method' => 'DELETE', 'name' => 'form'])!!}
-                    {!! Form::submit('Excluir', ['class' => 'btn btn-danger', $form??null]) !!}
+                    {!! Form::submit('Excluir', ['class' => 'btn form btn-danger', $form??null]) !!}
                     {!! Form::close() !!}
                 @endif
+            </div>
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -10,40 +10,42 @@
     </head>
     <body>
         <h1>Listagem de Contatos</h1>
-
-        <a type="button" class="btn btn-outline-primary" href="{{route('contatos.create')}}">+ Novo Contato</a>
-        <br><br/>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th id="coluna_nome" scope="col">Nome</th>
-                    <th scope="col">Categorias</th>
-                    <th scope="col">Telefones</th>
-                    <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($contatos as $contato)
-                <tr>
-                    <td>{{$contato->nome}}</td>
-                    <td>
-                        @foreach ($contato->categoria as $categoria)
-                            {{$categoria->nome}};
+        <div class="container">
+            <a type="button" class="btn btn-outline-primary" href="{{route('contatos.create')}}">+ Novo Contato</a>
+            <div class="card container">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th id="coluna_nome" scope="col">Nome</th>
+                            <th scope="col">Categorias</th>
+                            <th scope="col">Telefones</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($contatos as $contato)
+                        <tr>
+                            <td>{{$contato->nome}}</td>
+                            <td>
+                                @foreach ($contato->categoria as $categoria)
+                                    {{$categoria->nome}};
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach ($contato->telefone as $telefone)
+                                    {{$telefone->numero}} ({{$telefone->tipoTelefone->nome}});
+                                @endforeach
+                            </td>
+                            <td>
+                                <a type="button" class="btn btn-primary" href="{{route('contatos.show', $contato->id)}}">Visualizar</a>
+                                <a type="button" class="btn btn-warning" href="{{route('contatos.edit', $contato->id)}}">Editar</a>
+                            </td>
+                        </tr>
                         @endforeach
-                    </td>
-                    <td>
-                        @foreach ($contato->telefone as $telefone)
-                            {{$telefone->numero}} ({{$telefone->tipoTelefone->nome}});
-                        @endforeach
-                    </td>
-                    <td>
-                        <a type="button" class="btn btn-primary" href="{{route('contatos.show', $contato->id)}}">Visualizar</a>
-                        <a type="button" class="btn btn-warning" href="{{route('contatos.edit', $contato->id)}}">Editar</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    </tbody>
+                </table>
+            </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
